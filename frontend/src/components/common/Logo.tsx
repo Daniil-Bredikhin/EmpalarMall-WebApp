b3932a0eaa30a1d86a5f86fd4c9ddefd2debf051
Daniil-Bredikhin <daniilbredikhin@icloud.com>
2025-06-10 22:47:57 +0300

    refactor: убран логотип для Fullsize режима

--- frontend/src/components/common/Logo.tsx
+++ frontend/src/components/common/Logo.tsx
@@ -1,26 +1,5 @@
 import React from 'react';
-import logo from '../../assets/images/empalarmall_logo.png';
 
-const logoContainerStyle: React.CSSProperties = {
-  width: '100%',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  margin: '16px 0',
-  paddingTop: 'calc(env(safe-area-inset-top, 0px) + 60px)',
-};
-
-const logoImgStyle: React.CSSProperties = {
-  height: '20px',
-  maxWidth: '180px',
-  objectFit: 'contain',
-  display: 'block',
-};
-
-const Logo: React.FC = () => (
-  <div style={logoContainerStyle}>
-    <img src={logo} alt="EmpalarMall Logo" style={logoImgStyle} />
-  </div>
-);
+const Logo: React.FC = () => null;
 
 export default Logo; 
