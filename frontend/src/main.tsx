4ca89c5664e1b772b6bd129aa57cfefc32e33faa
Daniil-Bredikhin <daniilbredikhin@icloud.com>
2025-06-12 11:17:10 +0300

    fix: импорт стилей Ant Design v5 через reset.css для совместимости с Vite

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import 'antd/dist/antd.css';
+import 'antd/dist/reset.css';
 
 // Инициализация Telegram WebApp
 import WebApp from '@twa-dev/sdk'
